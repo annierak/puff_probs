2635a97c0f7c07962f6e8e51327dbdf31635102f
annierak <annie1rak@gmail.com>
2019-03-28 09:10:35 -0700

    hierarchy adjustment

---
+++ setup.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""Setup script for puff_probs package."""
+
+from setuptools import setup
+
+if __name__ == '__main__':
+    setup(name='puff_probs',
+          description='Very basic puff and fly objects',
+          author='Annie Rak',
+          url='https://github.com/annierak/puff_probs',
+          packages=['puff_probs'])
